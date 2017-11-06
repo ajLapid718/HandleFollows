0bc3fbce525edd4d57a9ef2a6d43f6845c7790ff
Allan James Lapid <ajLapid718@gmail.com>
2017-11-06 14:25:13 -0500

    Verify if a user wants to accept or reject all requests

--- 01-Get_Info.rb
+++ 01-Get_Info.rb
@@ -18,6 +18,21 @@ def get_password
   end
 end
 
+def accept_or_reject
+  begin
+    print "Enter letter A to accept all requests; enter letter R to reject all requests: "
+    decision = gets.chomp
+    if decision == "A" || "R"
+      return decision
+    else
+      raise Exception.new("Invalid entry. Please try again.")
+    end
+  rescue => e
+    puts "#{e}"
+    retry
+end
+
 # Invoke the Getting Info Process
 USERNAME = get_username
 PASSWORD = get_password
+DECISION = accept_or_reject
