047d6d137026091a1661fa6e11cdf0f34322053b
Allan James Lapid <ajLapid718@gmail.com>
2017-11-06 13:00:11 -0500

    Reject all follow requests

---
+++ 03-Handle_Follows.rb
@@ -0,0 +1,33 @@
+def click_on_heart
+  begin
+    @browser.a(:text => /^Activity Feed$/).click
+  rescue
+    puts "Moving on..."
+  end
+  "Successfully clicked on the heart..."
+end
+
+def open_follow_requests
+  begin
+    @browser.a(:text => /^Follow Requests$/).click
+  rescue
+    puts "Moving on..."
+  end
+  "Successfully opened your follow requests..."
+end
+
+def accept_all_or_reject_all
+  puts " "
+  puts "Currently rejecting all follow requests..."
+  amount_of_requests = @browser.li(:class => /^_75ljm _3qhgf$/).count
+  amount_of_requests.times do |counter|
+    begin
+      user = browser.a(:class => /^_2g7d5 notranslate _nodr2$/).text
+      puts "Currently denying a follow request from user: #{user}"
+      @browser.li(:class => /^_75ljm _3qhgf$/).button(:text => "Hide").click
+    rescue
+      puts "Moving on..."
+    end
+    puts "Successful. #{(amount_of_requests) - (counter + 1)} more requests left."
+  end
+end
